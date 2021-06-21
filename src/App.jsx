4f4fe9105e4db58817ad3fce62204837410c5722
mimiring <island_2@naver.com>
2021-06-22 00:05:29 +0900

    feat: 새로운 할 일 추가하는 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { useEffect, useState } from "react";
 
+const USER_ID = 123456789;
+const makeTodo = (title) => {
+  return {
+    id: Date.now(),
+    title,
+    completed: false,
+    userId: USER_ID,
+  };
+};
+
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [title, setTitle] = useState("");
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
@@ -9,12 +23,34 @@ function App() {
       .then((json) => setTodoList(json));
   }, []);
 
+  const handleSaveClick = () => {
+    if (title.length === 0) {
+      alert("값을 입력하세요");
+      return;
+    }
+    // todoList에 추가하기
+    setTodoList([
+      {
+        id: Date.now(),
+        title,
+        completed: false,
+        userId: USER_ID,
+      },
+      ...todoList,
+    ]);
+    setTitle("");
+  };
+
   return (
-    <ul>
-      {todoList.map((todoItem) => (
-        <li key={todoItem.id}>{todoItem.title}</li>
-      ))}
-    </ul>
+    <>
+      <input onChange={handleTitleChange} value={title} />
+      <button onClick={handleSaveClick}>저장</button>
+      <ul>
+        {todoList.map((todoItem) => (
+          <li key={todoItem.id}>{todoItem.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
