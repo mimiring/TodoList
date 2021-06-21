088d21cc6dc9c5bec8fe24574f34f6ac200b5b25
mimiring <island_2@naver.com>
2021-06-21 21:32:53 +0900

    feat: api에서 할 일 목록 데이터 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>App</div>;
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodoList(json));
+  }, []);
+
+  return (
+    <ul>
+      {todoList.map((todoItem) => (
+        <li key={todoItem.id}>{todoItem.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
