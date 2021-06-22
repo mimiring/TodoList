b5ad767c3837500a2eec48ed7305b193e6188640
mimiring <island_2@naver.com>
2021-06-22 22:49:18 +0900

    refactoring: request 파일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useEffect, useState } from "react";
 import AddForm from "./AddForm";
-
-const USER_ID = 123456789;
+import { addRequset, getAllRequest } from "./services/request";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
 
   const updateList = (title) => {
-    return fetch("https://jsonplaceholder.typicode.com/todos", {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-        completed: false,
-        userId: USER_ID,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((todoData) => {
-        setTodoList([todoData, ...todoList]);
-      });
+    return addRequset(title).then((todoData) => {
+      setTodoList([todoData, ...todoList]);
+    });
   };
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((json) => setTodoList(json));
+    getAllRequest().then((json) => setTodoList(json));
   }, []);
 
   return (
---
+++ src/services/request.js
@@ -0,0 +1,22 @@
+const URL = "https://jsonplaceholder.typicode.com/todos";
+const POST = "POST";
+
+export const getAllRequest = () => {
+  return fetch(URL).then((response) => response.json());
+};
+
+const USER_ID = 123456789;
+
+export const addRequset = (title) => {
+  return fetch(URL, {
+    method: POST,
+    body: JSON.stringify({
+      title,
+      completed: false,
+      userId: USER_ID,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((response) => response.json());
+};
