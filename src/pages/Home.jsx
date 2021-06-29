740fa974fd3ccdfa9217ee598d7a3e48e564d3aa
mimiring <island_2@naver.com>
2021-06-29 21:34:40 +0900

    feat: [#7] home, new, detail, 404 페이지 컴포넌트 생성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import {
+  addRequset,
+  deleteTodoRequest,
+  getAllRequest,
+  updateRequest,
+} from "./services/request";
+import TodoList from "./components/TodoList";
+import AddForm from "./components/AddForm";
+import UpdateForm from "./components/UpdateForm";
+import { STATUS } from "./constants";
+
+function Home() {
+  const [todoList, setTodoList] = useState([]);
+  const [target, setTarget] = useState(null);
+  const [isUpdateVisible, setUpdateVisible] = useState(false);
+
+  const addTodo = (todo) => {
+    return addRequset(todo).then((todoData) => {
+      setTodoList([todoData, ...todoList]);
+    });
+  };
+
+  const updateTodo = (newTodo) => {
+    updateRequest(newTodo).then(() => {
+      const newTodoList = todoList.map((todo) => {
+        if (todo.id === newTodo.id) {
+          return newTodo;
+        }
+        return todo;
+      });
+      setTodoList(newTodoList);
+      setTarget(null);
+    });
+  };
+
+  const deleteTodo = (todoId) => {
+    deleteTodoRequest(todoId).then(() => {
+      const newTodoList = todoList.filter((todo) => {
+        return todo.id !== todoId;
+      });
+      setTodoList(newTodoList);
+    });
+  };
+
+  const openEditForm = (todo) => {
+    setTarget(todo);
+    setUpdateVisible(true);
+  };
+
+  const closeEditForm = () => {
+    setUpdateVisible(false);
+  };
+
+  useEffect(() => {
+    getAllRequest().then((json) => setTodoList(json));
+  }, []);
+
+  return (
+    <>
+      <AddForm onSave={addTodo} />
+      {isUpdateVisible && (
+        <UpdateForm
+          todo={target}
+          onUpdate={updateTodo}
+          onClose={closeEditForm}
+        />
+      )}
+      <h2>To Do</h2>
+      <TodoList
+        todoList={todoList.filter((todo) => todo.status === STATUS.TODO)}
+        message="할 일을 모두 마쳤어요"
+        onDelete={deleteTodo}
+        onEditClick={openEditForm}
+      />
+      {console.log()}
+      <h2>In Progress</h2>
+      <TodoList
+        todoList={todoList.filter((todo) => todo.status === STATUS.INPROGRESS)}
+        message="진행중인 일을 모두 마쳤어요"
+        onDelete={deleteTodo}
+        onEditClick={openEditForm}
+      />
+      <h2>Done</h2>
+      <TodoList
+        todoList={todoList.filter((todo) => todo.status === STATUS.DONE)}
+        message="완료한 일이 아직 없네요"
+        onDelete={deleteTodo}
+        onEditClick={openEditForm}
+      />
+    </>
+  );
+}
+
+export default Home;
