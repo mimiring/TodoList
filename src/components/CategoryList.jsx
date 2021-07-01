9c0657f41ea9494612a1c1c25a27b142a82ac0e1
mimiring <island_2@naver.com>
2021-07-02 01:43:18 +0900

    feat: [#8] 카테고리 목록 UI 디자인 적용

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CATEGORY } from "../constants";
+
+function CategoryItem({ name, count }) {
+  let emoji = "";
+  if (name === CATEGORY.WORK) {
+    emoji = "🖥";
+  } else if (name === CATEGORY.SELF_DEVELOPMENT) {
+    emoji = "🥰";
+  } else if (name === CATEGORY.HOBBY) {
+    emoji = "❤";
+  } else if (name === CATEGORY.HOUSE_WORK) {
+    emoji = "🏠";
+  } else if (name === CATEGORY.ETC) {
+    emoji = "🗒";
+  }
+
+  return (
+    <li className="category_item">
+      <Link to={`/?categories=${name}`}>
+        <div className="category_item_emoji">{emoji}</div>
+        <span className="category_item_name">{name}</span>
+        <span className="category_item_count">{count}</span>
+      </Link>
+    </li>
+  );
+}
 
 function CategoryList({ categories }) {
   return (
-    <ul>
-      {categories.map(([key, value]) => (
-        <li>
-          <Link to={`/?categories=${key}`}>
-            <span class="category_item_name">{key}</span>
-            <span class="category_item_count">{value}</span>
-          </Link>
-        </li>
+    <ul className="category_list">
+      {categories.map(([name, count]) => (
+        <CategoryItem name={name} count={count} />
       ))}
     </ul>
   );
