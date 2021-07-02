cfd5baa2463c0214f771e752781223810448842f
mimiring <island_2@naver.com>
2021-07-03 01:19:12 +0900

    feat: [#10] Search 컴포넌트 추가

---
+++ src/components/Search.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { searchRequest } from "../services/request";
+
+const Search = () => {
+  const [keyword, setKeyword] = useState();
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (e) => {
+    const keyword = e.target.value;
+    setKeyword(keyword);
+
+    if (keyword.trim().length > 0) {
+      searchRequest(keyword).then((json) => setTodoList(json));
+    }
+  };
+
+  return (
+    <div>
+      <input onChange={handleChange} value={keyword} />
+      {todoList.length > 0 && (
+        <ul>
+          {todoList.map((todo) => (
+            <li>
+              {todo.category} {todo.title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Search;
