f615d76c457cd4f908bfda89e4977250aed400df
mimiring <island_2@naver.com>
2021-07-05 00:55:48 +0900

    feat: [#8] 404 에러 페이지 스타일 적용

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const NotFound = () => {
-  return <div>404</div>;
-};
+function NotFound() {
+  return (
+    <div>
+      <div className="error_wrap">
+        <h1 className="error_code">404</h1>
+        <div className="error_message">Page Not Found</div>
+      </div>
+      <button className="backbtn">Back to Home</button>
+    </div>
+  );
+}
 
 export default NotFound;
