7318d9e528110ff4d253b74327af5b610623ac46
mimiring <island_2@naver.com>
2021-07-09 22:42:58 +0900

    refactoring: New 컴포넌트 타입 지정

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 import AddForm from "../components/AddForm";
+import { ToDo } from "../components/Todo";
 import { addRequset } from "../services/request";
 
-const New = ({ history }) => {
+type NewProps = RouteComponentProps;
+
+const New = ({ history }: NewProps) => {
   const handleBackClick = () => {
     history.push("/");
   };
 
-  const addTodo = (todo) => {
+  const addTodo = (todo: ToDo) => {
     return addRequset(todo)
       .then(() => {
         history.push("/");
@@ -21,7 +25,7 @@ const New = ({ history }) => {
     <div>
       <div className="todo_title_wrap">
         <button className="todo_backbtn" onClick={handleBackClick}></button>
-        <h3 class="todo_title">New To Do</h3>
+        <h3 className="todo_title">New To Do</h3>
       </div>
       <AddForm onSave={addTodo} />
     </div>
